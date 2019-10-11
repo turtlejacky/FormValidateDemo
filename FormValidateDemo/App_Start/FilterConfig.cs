34345389fa38f17dfbf05eb27533bb9b636c947d
NeilTao <jack370434@gmail.com>
2019-10-11 16:16:23 +0800

    Add project files.

---
+++ FormValidateDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FormValidateDemo
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
